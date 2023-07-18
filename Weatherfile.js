f6fcbfa9c5d57c27c70786f7c41692ce05a52dab
Usama Awan <98221698+USAMA-NUSRAT@users.noreply.github.com>
2023-07-18 23:06:18 +0500

    Update Weatherfile.js

--- Weatherfile.js
+++ Weatherfile.js
@@ -6,6 +6,7 @@ function Weatherfile() {
   const [dat, setDat] = useState({});
   const [icn, setIcn] = useState("");
   const [isLogIn, setIsLogIn] = useState(false);
+  const [isLogout, setIsLogout] = useState(false);
   let data;
   const getApiData = async () => {
     // try{
